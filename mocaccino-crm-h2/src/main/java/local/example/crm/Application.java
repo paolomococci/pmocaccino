d6e987fa123ab104da57ff34ef465dadf1099213
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-13 08:48:49 +0200

    Update Application.java

--- mocaccino-crm-h2/src/main/java/local/example/crm/Application.java
+++ mocaccino-crm-h2/src/main/java/local/example/crm/Application.java
@@ -20,8 +20,9 @@ package local.example.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
