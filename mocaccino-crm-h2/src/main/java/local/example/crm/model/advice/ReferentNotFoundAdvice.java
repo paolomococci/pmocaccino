6c591664f1cf3e9e86859be0b377f5c95ffc0b7d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-15 23:45:00 +0200

    Create ReferentNotFoundAdvice.java

---
+++ mocaccino-crm-h2/src/main/java/local/example/crm/model/advice/ReferentNotFoundAdvice.java
@@ -0,0 +1,38 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.crm.model.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import local.example.crm.model.exception.ReferentNotFoundException;
+
+@ControllerAdvice
+public class ReferentNotFoundAdvice {
+
+	@ResponseBody
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String referentNotFoundHandler(ReferentNotFoundException referentNotFoundException) {
+		return referentNotFoundException.getMessage();
+	}
+}
