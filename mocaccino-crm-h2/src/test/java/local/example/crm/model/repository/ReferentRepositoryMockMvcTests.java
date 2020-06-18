600b0e36e61b17508e14fe1bcf907764a3680d4c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-06-18 23:52:54 +0200

    Create ReferentRepositoryMockMvcTests.java

---
+++ mocaccino-crm-h2/src/test/java/local/example/crm/model/repository/ReferentRepositoryMockMvcTests.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.crm.model.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReferentRepositoryMockMvcTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	ReferentRepository referentRepository;
+
+	@Test
+	void contextTest() 
+			throws Exception {
+		assertThat(referentRepository).isNotNull();
+	}
+}
