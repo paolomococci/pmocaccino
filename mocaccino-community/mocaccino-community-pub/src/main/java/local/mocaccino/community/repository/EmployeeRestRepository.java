02e1fe6e1445c48481d4aca863b7b925638716fd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-11 07:23:49 +0100

    Create EmployeeRestRepository.java

---
+++ mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/repository/EmployeeRestRepository.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.mocaccino.community.repository;
+
+import local.mocaccino.community.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface EmployeeRestRepository
+		extends JpaRepository<Employee, Long> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE NAME LIKE ?1%")
+	List<Employee> likeByName(String name);
+
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE SURNAME LIKE ?1%")
+	List<Employee> likeBySurname(String surname);
+
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE EMAIL LIKE ?1%")
+	List<Employee> likeByEmail(String email);
+
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE USERNAME LIKE ?1%")
+	List<Employee> likeByUsername(String username);
+}
