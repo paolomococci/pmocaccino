7a5149b11d7f6628cfe2a7eabfd82d7ab9a9a869
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-12 04:29:30 +0100

    Create Company.java

---
+++ mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/model/Company.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.mocaccino.community.model;
+
+import local.mocaccino.community.validator.constraint.AlphaConstraint;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "COMPANY",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"NAME"}))
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @AlphaConstraint
+    private String name;
+
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    private Employee employee;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    private Set<Contest> contests;
+}
