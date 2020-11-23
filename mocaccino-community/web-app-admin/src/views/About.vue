6bcb3ee76f452c1d03ce5b096f1f6ab9956e89af
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-23 23:32:01 +0100

    Create About.vue

---
+++ mocaccino-community/web-app-admin/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="about">
+    <welcome message="this is about page"/>
+  </section>
+</template>
+
+<script>
+import Welcome from '@/components/Welcome'
+
+export default {
+  name: 'About',
+  components: {
+    welcome: Welcome
+  }
+}
+</script>
