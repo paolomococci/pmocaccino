8a892c7735277ca358043e5c491513e4656754e0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-23 23:32:24 +0100

    Create Home.vue

---
+++ mocaccino-community/web-app-admin/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="home">
+    <welcome message="welcome to this example of Vue.js web application"/>
+  </section>
+</template>
+
+<script>
+import Welcome from '@/components/Welcome'
+
+export default {
+  name: 'Home',
+  components: {
+    welcome: Welcome
+  }
+}
+</script>
