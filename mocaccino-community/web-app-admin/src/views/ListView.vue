0adab1303d59d4adcb4070e7e334d41064eb62a4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-24 22:26:09 +0100

    Create ListView.vue

---
+++ mocaccino-community/web-app-admin/src/views/ListView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <listing message="this page shows the list of employees already registered"/>
+  </section>
+</template>
+
+<script>
+import ListEmployeesComponent from '@/components/ListEmployeesComponent'
+
+export default {
+  name: 'ListView',
+  components: {
+    listing: ListEmployeesComponent
+  }
+}
+</script>
