627ba4173b56032b5888e04f66bd015f436fca8d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-24 23:01:44 +0100

    Create EmployeeDetailsView.vue

---
+++ mocaccino-community/web-app-admin/src/views/EmployeeDetailsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <detail message="on this page you can view the registered data of an employee"/>
+  </section>
+</template>
+
+<script>
+import EmployeeDetailsComponent from '@/components/EmployeeDetailsComponent'
+
+export default {
+  name: 'EmployeeDetailsView',
+  components: {
+    detail: EmployeeDetailsComponent
+  }
+}
+</script>
