19ef96be56d8b3a6cc15f394f25572f5d9020ddd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-24 22:18:19 +0100

    Create AddView.vue

---
+++ mocaccino-community/web-app-admin/src/views/AddView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <adding message="this is adding page"/>
+  </section>
+</template>
+
+<script>
+import AddEmployeeComponent from '@/components/AddEmployeeComponent'
+
+export default {
+  name: 'AddView',
+  components: {
+    adding: AddEmployeeComponent
+  }
+}
+</script>
