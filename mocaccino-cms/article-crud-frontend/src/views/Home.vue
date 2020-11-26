2ae8ba3fea71063167f867a86db692f24f68e171
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-26 19:20:22 +0100

    Create Home.vue

---
+++ mocaccino-cms/article-crud-frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="home">
+    <hello msg="home page"/>
+  </section>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    hello: HelloWorld
+  }
+}
+</script>
