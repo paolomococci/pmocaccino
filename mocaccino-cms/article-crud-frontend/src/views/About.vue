ba2485d543544652831df86dcc2d094495286975
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-26 19:21:04 +0100

    Create About.vue

---
+++ mocaccino-cms/article-crud-frontend/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="about">
+    <hello msg="about page"/>
+  </section>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'About',
+  components: {
+    hello: HelloWorld
+  }
+}
+</script>
