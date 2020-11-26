e1fbfb5144bd3f854f74917ecf545263ad55a773
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-26 06:24:01 +0100

    Create ArticleRepository.java

---
+++ mocaccino-cms/article-crud-pub/src/main/java/local/mocaccino/cms/repository/ArticleRepository.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.mocaccino.cms.repository;
+
+import local.mocaccino.cms.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface ArticleRepository
+        extends JpaRepository<Article, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM ARTICLE WHERE TITLE LIKE ?1%")
+    List<Article> likeByTitle(String title);
+}
