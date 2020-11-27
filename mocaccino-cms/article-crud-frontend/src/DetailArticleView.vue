d343db9885dfa4d616dc88a9e33ebffd9e75f09f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-27 06:55:23 +0100

    Create DetailArticleView.vue

---
+++ mocaccino-cms/article-crud-frontend/src/DetailArticleView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <welcome msg="detail page"/>
+  </section>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'DetailArticleView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
