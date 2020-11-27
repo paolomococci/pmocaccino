01a6305636b9ccee2df1da84e443fd90d3d2e903
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-27 06:51:57 +0100

    Create DetailArticleComponent.vue

---
+++ mocaccino-cms/article-crud-frontend/src/components/DetailArticleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="hello">
+    <mark>{{msg}}</mark>
+  </section>
+</template>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4b8;
+}
+</style>
+
+<script>
+
+export default {
+  name: 'DetailArticleComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
