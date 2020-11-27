6537e598f965004b9213beeaefda75ea6f74eddf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-27 06:50:57 +0100

    Create AddArticleComponent.vue

---
+++ mocaccino-cms/article-crud-frontend/src/components/AddArticleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <mark>{{msg}}</mark>
+  </section>
+</template>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4b8;
+}
+</style>
+
+<script>
+
+export default {
+  name: 'AddArticleComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
