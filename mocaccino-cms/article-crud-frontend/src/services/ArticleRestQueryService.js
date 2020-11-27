2ce55eb6bf93e755d250852d418d809d59bbdfbf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-27 07:00:05 +0100

    Update ArticleRestQueryService.js

--- mocaccino-cms/article-crud-frontend/src/services/ArticleRestQueryService.js
+++ mocaccino-cms/article-crud-frontend/src/services/ArticleRestQueryService.js
@@ -1,6 +1,31 @@
 import query-config from '../config/query-config.js'
 
 class ArticleRestQueryService {
+
+  create() {
+    // TODO
+  }
+
+  read() {
+    // TODO
+  }
+
+  readAll() {
+    // TODO
+  }
+
+  update() {
+    // TODO
+  }
+
+  delete() {
+    // TODO
+  }
+
+  find() {
+    // TODO
+  }
+
   constructor() {
 
   }
