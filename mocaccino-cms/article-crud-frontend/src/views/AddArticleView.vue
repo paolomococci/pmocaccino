f111d03d63a491d94ceaa4dc70f97fb2a127588a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-27 06:53:54 +0100

    Create AddArticleView.vue

---
+++ mocaccino-cms/article-crud-frontend/src/views/AddArticleView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <welcome msg="add page"/>
+  </section>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'AddArticleView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
