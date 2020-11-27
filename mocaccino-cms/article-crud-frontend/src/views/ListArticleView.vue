f0e189f445ef9e1703f938efc0c5c42f3f22aa9e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-27 06:54:47 +0100

    Create ListArticleView.vue

---
+++ mocaccino-cms/article-crud-frontend/src/views/ListArticleView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <welcome msg="list page"/>
+  </section>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'ListArticleView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
