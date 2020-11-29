f70cf70ff37715d9e380f768d568fe0f6de20cca
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 10:30:10 +0100

    Create GreetingComponent.vue

---
+++ greeting/greeting-frontend/src/components/GreetingComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="hello">
+    <h1>{{msg}}</h1>
+  </article>
+</template>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4b8;
+}
+</style>
+
+<script>
+export default {
+  name: 'GreetingComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
