a6c905a3c2f7ce7b027993045533f9c532b27341
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 10:31:00 +0100

    Create HomeView.vue

---
+++ greeting/greeting-frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="home">
+    <welcome msg="home page"/>
+  </section>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
