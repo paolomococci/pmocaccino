eb8d3a5d1c36162fbea7cecb397fb6995fee88e6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 20:14:52 +0100

    Update GreetingComponent.vue

--- greeting/greeting-frontend/src/components/GreetingComponent.vue
+++ greeting/greeting-frontend/src/components/GreetingComponent.vue
@@ -1,31 +1,49 @@
 <template>
   <article class="hello">
     <h1>{{msg}}</h1>
+    <h2><em>response from REST web service at url:</em> {{url}}</h2>
+    <h3>{{entities}}</h3>
   </article>
 </template>
 
 <style scoped lang="scss">
+h1 {
+  font-size: 30px;
+  margin: 50px 0 0;
+}
 h3 {
+  font-size: 16px;
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #4b8;
+h3 {
+  font-size: 14px;
+  margin: 30px 0 0;
 }
 </style>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'GreetingComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    url: 'http://localhost:8090/api/greeting?name=John',
+    entities: null
+  }),
+  mounted () {
+    axios.get(this.url)
+      .then(response => {
+        this.entities = response.data
+        this.handleResponse(response)
+      })
+  },
+  methods: {
+    handleResponse (response) {
+      console.log(response)
+    }
   }
 }
 </script>
