ac6e38f4510b8b669b1fea4f5d178c9a48bc7dbf
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 20:16:00 +0100

    Update GreetingComponent.vue

--- greeting/greeting-frontend/src/components/GreetingComponent.vue
+++ greeting/greeting-frontend/src/components/GreetingComponent.vue
@@ -2,7 +2,7 @@
   <article class="hello">
     <h1>{{msg}}</h1>
     <h2><em>response from REST web service at url:</em> {{url}}</h2>
-    <h3>{{entities}}</h3>
+    <p><code>{{entities}}</code></p>
   </article>
 </template>
 
@@ -15,7 +15,7 @@ h3 {
   font-size: 16px;
   margin: 40px 0 0;
 }
-h3 {
+p {
   font-size: 14px;
   margin: 30px 0 0;
 }
