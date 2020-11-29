07291649a987cb232458850f0f8d9eaa15f22d62
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 20:12:08 +0100

    Update WelcomeComponent.vue

--- greeting/greeting-frontend/src/components/WelcomeComponent.vue
+++ greeting/greeting-frontend/src/components/WelcomeComponent.vue
@@ -5,19 +5,9 @@
 </template>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #4b8;
+h1 {
+  font-size: 30px;
+  margin: 50px 0 0;
 }
 </style>
 
