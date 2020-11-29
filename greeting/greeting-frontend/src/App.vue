6bce3baedc54471f7362a8f135d8e22e4b0c8d89
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 10:32:45 +0100

    Create App.vue

---
+++ greeting/greeting-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main id="app">
+    <nav id="nav">
+      <router-link to="/">home</router-link> &#173;
+      <router-link to="/about">about</router-link> &#173;
+      <router-link to="/greeting">greeting</router-link>
+    </nav>
+    <router-view/>
+  </main>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #235;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #235;
+
+    &.router-link-exact-active {
+      color: #4b8;
+    }
+  }
+}
+</style>
