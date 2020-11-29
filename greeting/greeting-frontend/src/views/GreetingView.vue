df835dd4495de3c9c45a2026b0aca4e5bb83dac5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 10:32:00 +0100

    Create GreetingView.vue

---
+++ greeting/greeting-frontend/src/views/GreetingView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <greeting msg="greeting page todo"/>
+  </section>
+</template>
+
+<script>
+import GreetingComponent from '@/components/GreetingComponent.vue'
+
+export default {
+  name: 'GreetingView',
+  components: {
+    greeting: GreetingComponent
+  }
+}
+</script>
