4dadf28512e07b27c7ef8291fe04d127c39800de
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 10:31:30 +0100

    Create AboutView.vue

---
+++ greeting/greeting-frontend/src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="about">
+    <welcome msg="about page"/>
+  </section>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
