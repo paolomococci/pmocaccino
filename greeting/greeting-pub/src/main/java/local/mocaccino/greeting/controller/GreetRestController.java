1593d674c17686392694fb2808c1c71eaca62a48
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-11-29 10:47:20 +0100

    Create GreetRestController.java

---
+++ greeting/greeting-pub/src/main/java/local/mocaccino/greeting/controller/GreetRestController.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.mocaccino.greeting.controller;
+
+import local.mocaccino.greeting.model.Greet;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(maxAge = 3600)
+public class GreetRestController {
+
+    private static final String template = "Hi, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    @CrossOrigin(origins = "http://127.0.0.1:8010")
+    public Greet greeting(@RequestParam(required = false, defaultValue = "user") String name) {
+        return new Greet(counter.incrementAndGet(), String.format(template, name));
+    }
+}
