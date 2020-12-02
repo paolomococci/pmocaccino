b1e45e9ae6bc481cca35e51923684112dc73f1c0
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-02 09:31:43 +0100

    Update GreetingComponent.vue

--- greeting/greeting-frontend/src/components/GreetingComponent.vue
+++ greeting/greeting-frontend/src/components/GreetingComponent.vue
@@ -1,9 +1,15 @@
 <template>
-  <article class="hello">
+  <section class="hello">
     <h1>{{msg}}</h1>
     <h2><em>response from REST web service at url:</em> {{url}}</h2>
-    <p><code>{{entities}}</code></p>
-  </article>
+    <h3>raw data retrieved</h3>
+    <p><code>{{dataRetrieved}}</code></p>
+    <h3>data retrieved</h3>
+    <p>
+      ID: <tt>{{dataRetrieved.id}}</tt><br/>
+      message: <tt>{{dataRetrieved.message}}</tt>
+    </p>
+  </section>
 </template>
 
 <style scoped lang="scss">
@@ -11,13 +17,17 @@ h1 {
   font-size: 30px;
   margin: 50px 0 0;
 }
-h3 {
+h2 {
   font-size: 16px;
   margin: 40px 0 0;
 }
-p {
+h3 {
   font-size: 14px;
   margin: 30px 0 0;
+}
+p {
+  font-size: 14px;
+  margin: 20px 0 0;
 }
 </style>
 
@@ -31,12 +41,12 @@ export default {
   },
   data: () => ({
     url: 'http://localhost:8090/api/greeting?name=John',
-    entities: null
+    dataRetrieved: {id: 0, message: ''}
   }),
   mounted () {
     axios.get(this.url)
       .then(response => {
-        this.entities = response.data
+        this.dataRetrieved = response.data
         this.handleResponse(response)
       })
   },
