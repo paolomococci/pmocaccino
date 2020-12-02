ab604ce07af80688ae8238630c839f7d8756efa7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-02 16:24:15 +0100

    Update ListArticleComponent.vue

--- mocaccino-cms/article-crud-frontend/src/components/ListArticleComponent.vue
+++ mocaccino-cms/article-crud-frontend/src/components/ListArticleComponent.vue
@@ -27,6 +27,17 @@ export default {
   name: 'ListArticleComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    url: 'http://localhost:8090/rest/articles',
+    articles: [],
+    currentArticle: {id: 0, title: '', description: '', publisched: false},
+    currentIndex: -1,
+    title: ''
+  }),
+  methods: {
+  },
+  mounted() {
   }
 }
 </script>
