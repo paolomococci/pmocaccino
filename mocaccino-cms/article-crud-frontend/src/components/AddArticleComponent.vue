b040b270f9de8af7e9bbf650a73b2e14c05a0614
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-03 20:58:36 +0100

    Update AddArticleComponent.vue

--- mocaccino-cms/article-crud-frontend/src/components/AddArticleComponent.vue
+++ mocaccino-cms/article-crud-frontend/src/components/AddArticleComponent.vue
@@ -1,32 +1,63 @@
 <template>
   <section>
-    <mark>{{msg}}</mark>
+    <h1>{{msg}}</h1>
+    <aside v-if="!submitted">
+        <label for="title">title</label>
+        <input type="text" id="title" required v-model="article.title" name="title"/>
+        <label for="description">description</label>
+        <input id="description" required v-model="article.description" name="description"/>
+      <button @click="createArticle">submit</button>
+    </aside>
+    <aside v-else>
+      <h4>you submitted successfully</h4>
+      <button @click="newArticle">Add</button>
+    </aside>
   </section>
 </template>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #4b8;
-}
+  h1 {
+    font-size: 20px;
+    font-weight: lighter;
+    margin: 40px;
+  }
+  label, input, button {
+    margin: 5px;
+  }
 </style>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'AddArticleComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    article: {title: '', description: '', published: false},
+    submitted: false
+  }),
+  methods: {
+    createArticle() {
+      var data = {
+        title: this.article.title,
+        description: this.article.description,
+        published: false
+      };
+      axios.post("http://localhost:8090/rest/articles", data)
+        .then(response => {
+          console.log(response.data);
+          this.submitted = true;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    newArticle() {
+      this.submitted = false;
+      this.article = {};
+    }
   }
-}
+};
 </script>
