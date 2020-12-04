b7fde232cdf88889d75c40478f26ee8544fcf411
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-04 18:50:38 +0100

    Create App.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <main id="app">
+    <nav id="nav">
+      <router-link class="routerLink" to="/">home</router-link>
+      <router-link class="routerLink" to="/about">about</router-link>
+    </nav>
+    <router-view/>
+  </main>
+</template>
+
+<style lang="scss">
+.routerLink {
+  margin: 5px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #235;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #235;
+
+    &.router-link-exact-active {
+      color: #4b8;
+    }
+  }
+}
+</style>
