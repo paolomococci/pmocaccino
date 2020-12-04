7bd2e4ff88fa9ad9deb9240ed90b169ea63ae4b2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-04 18:54:34 +0100

    Create WelcomeComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/WelcomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>{{msg}}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+  margin: 40px;
+}
+</style>
