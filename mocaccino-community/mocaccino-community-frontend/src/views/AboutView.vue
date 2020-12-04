6c8b569bb8e20b04e6fec3c23f95269bc46b9194
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-04 18:54:04 +0100

    Create AboutView.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <article>
+    <welcome msg="about view"/>
+  </article>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
