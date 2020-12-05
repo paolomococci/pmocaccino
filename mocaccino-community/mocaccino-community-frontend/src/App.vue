a195b161d7f8e1086ffe3e002d8420a3e3593c5e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-05 20:46:04 +0100

    Update App.vue

--- mocaccino-community/mocaccino-community-frontend/src/App.vue
+++ mocaccino-community/mocaccino-community-frontend/src/App.vue
@@ -1,34 +1,18 @@
 <template>
   <div id="app">
     <nav id="nav">
-      <router-link class="routerLink" to="/">home</router-link>
-      <router-link class="routerLink" to="/about">about</router-link>
+      <headerComponent/>
     </nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-.routerLink {
-  margin: 5px;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #235;
-}
-#nav {
-  padding: 30px;
+<script>
+import HeaderComponent from '@/components/HeaderComponent'
 
-  a {
-    font-weight: bold;
-    color: #235;
-
-    &.router-link-exact-active {
-      color: #4b8;
-    }
+export default {
+  components: {
+    headerComponent: HeaderComponent
   }
 }
-</style>
+</script>
