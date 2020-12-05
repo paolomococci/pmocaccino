439804a28c37713e1ae9820455645dde9514bfed
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-05 07:18:49 +0100

    Create HeaderComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <nav>
+      <!-- TODO-->
+    </nav>
+    <aside>
+      <!-- TODO-->
+    </aside>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss">
+
+</style>
