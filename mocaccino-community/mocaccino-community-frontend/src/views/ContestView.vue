45fe5e2e5b077be920de4538be2dbf9e4449a197
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-05 20:58:46 +0100

    Update ContestView.vue

--- mocaccino-community/mocaccino-community-frontend/src/views/ContestView.vue
+++ mocaccino-community/mocaccino-community-frontend/src/views/ContestView.vue
@@ -1,12 +1,16 @@
 <template>
   <main>
-    <!-- TODO -->
+    <welcomeComponent msg="contest view"/>
   </main>
 </template>
 
 <script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
 
 export default {
-  name: 'ContestView'
+  name: 'ContestView',
+  components: {
+    welcomeComponent: WelcomeComponent
+  }
 }
 </script>
