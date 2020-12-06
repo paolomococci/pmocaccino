0cbfe12953160a5aec6715083635ccde6b6dbc87
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-06 20:54:15 +0100

    Update CompanyView.vue

--- mocaccino-community/mocaccino-community-frontend/src/views/CompanyView.vue
+++ mocaccino-community/mocaccino-community-frontend/src/views/CompanyView.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <welcomeComponent msg="company view"/>
+    <companyComponent/>
   </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import CompanyComponent from '@/components/CompanyComponent.vue'
 
 export default {
   name: 'CompanyView',
   components: {
-    welcomeComponent: WelcomeComponent
+    welcomeComponent: WelcomeComponent,
+    companyComponent: CompanyComponent
   },
   mounted () {
     this.$store.state.placeholderSearch = 'type a name to search for',
