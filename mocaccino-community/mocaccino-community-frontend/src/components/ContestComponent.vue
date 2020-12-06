e653d066e641e4c565b22fe70405d16d671cb650
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-06 12:07:56 +0100

    Update ContestComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/ContestComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/ContestComponent.vue
@@ -5,11 +5,41 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'ContestComponent'
+  name: 'ContestComponent',
+  data: () => ({
+    url: 'http://localhost:8090/rest/contests',
+    contests: [],
+    contest: {name: '', title: '', description: '', date:  null, _links: {self: {href: ''}}},
+    index: 0,
+    title: ''
+  }),
+  methods: {
+    retrieveContests() {
+      axios.get(this.url)
+        .then(response => {
+          this.contests = response.data._embedded.contests;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    searchByTitle() {
+      axios.get(`http://localhost:8090/rest/articles/search/likeByTitle?title=${this.title}`)
+        .then(response => {
+          this.contests = response.data._embedded.contests;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    // TODO
+  }
 }
 </script>
-
-<style lang="scss">
-
-</style>
