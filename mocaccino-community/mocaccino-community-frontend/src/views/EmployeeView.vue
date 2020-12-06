c9bee93ab4db89955d08195ce27c2cb3b31579e2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-06 12:15:48 +0100

    Update EmployeeView.vue

--- mocaccino-community/mocaccino-community-frontend/src/views/EmployeeView.vue
+++ mocaccino-community/mocaccino-community-frontend/src/views/EmployeeView.vue
@@ -13,7 +13,7 @@ export default {
     welcomeComponent: WelcomeComponent
   },
   mounted () {
-    this.$store.state.placeholderSearch = 'type a username to search',
+    this.$store.state.placeholderSearch = 'type a username to search for',
     this.$store.state.disabledSearchAvailability = false
   },
   destroyed () {
