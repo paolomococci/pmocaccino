feb33e33382fafb4f9cd042c51c44e34c93da390
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-06 12:08:27 +0100

    Update EmployeeComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeComponent.vue
@@ -5,11 +5,41 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'EmployeeComponent'
+  name: 'EmployeeComponent',
+  data: () => ({
+    url: 'http://localhost:8090/rest/employees',
+    employees: [],
+    employee: {name: '', surname: '', email: '', profession: '', username: '', _links: {self: {href: ''}}},
+    index: 0,
+    username: ''
+  }),
+  methods: {
+    retrieveEmployees() {
+      axios.get(this.url)
+        .then(response => {
+          this.employees = response.data._embedded.employees;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    searchByEmail() {
+      axios.get(`http://localhost:8090/rest/employees/search/likeByUsername?username=${this.username}`)
+        .then(response => {
+          this.employees = response.data._embedded.employees;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    // TODO
+  }
 }
 </script>
-
-<style lang="scss">
-
-</style>
