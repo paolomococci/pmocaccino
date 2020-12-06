f8a885d1b8373b157cfac56b6794ec1b7a6676ce
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-06 12:07:20 +0100

    Update CompanyComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyComponent.vue
@@ -5,11 +5,41 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'CompanyComponent'
+  name: 'CompanyComponent',
+  data: () => ({
+    url: 'http://localhost:8090/rest/companies',
+    companies: [],
+    company: {name: '', _links: {self: {href: ''}}},
+    index: 0,
+    name: ''
+  }),
+  methods: {
+    retrieveCompanies() {
+      axios.get(this.url)
+        .then(response => {
+          this.companies = response.data._embedded.companies;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    searchByTitle() {
+      axios.get(`http://localhost:8090/rest/articles/search/likeByName?name=${this.name}`)
+        .then(response => {
+          this.companies = response.data._embedded.companies;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    // TODO
+  }
 }
 </script>
-
-<style lang="scss">
-
-</style>
