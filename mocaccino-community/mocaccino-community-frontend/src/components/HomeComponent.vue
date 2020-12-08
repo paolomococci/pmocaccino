fcd3f41e8df34b83d3625d2aaead485620b38b85
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-09 00:04:38 +0100

    Update HomeComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/HomeComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/HomeComponent.vue
@@ -3,13 +3,12 @@
     <b-jumbotron>
       <template #header>in this web application</template>
       <hr class="my-4">
-      <p><span v-text="msg"></p>
-      <b-button variant="primary" href="#/company">company</b-button>
-      <span> &#173; </span>
-      <b-button variant="primary" href="#/contest">contest</b-button>
-      <span> &#173; </span>
-      <b-button variant="primary" href="#/employee">employee</b-button>
-      <span> &#173; </span>
+      <p v-text="msg"></p>
+      <b-row>
+        <b-col><b-button size="lg" variant="outline-secondary" href="#/company">company</b-button></b-col>
+        <b-col><b-button size="lg" variant="outline-secondary" href="#/contest">contest</b-button></b-col>
+        <b-col><b-button size="lg" variant="outline-secondary" href="#/employee">employee</b-button></b-col>
+      </b-row>
     </b-jumbotron>
   </section>
 </template>
