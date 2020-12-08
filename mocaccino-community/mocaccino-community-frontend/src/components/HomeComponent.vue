40402629c607e02e6c653bbff66e4be5a131523d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-08 11:29:12 +0100

    Update HomeComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/HomeComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/HomeComponent.vue
@@ -1,1 +1,50 @@
+<template>
+  <section>
+    <h3><span v-text="msg"></span></h3>
+  </section>
+</template>
 
+<script>
+export default {
+  name: 'HomeComponent',
+  data: () => ({
+    // TODO
+  }),
+  props: {
+    msg: String
+  },
+  components: {
+    // TODO
+  },
+  filters: {
+    // TODO
+  },
+  watch: {
+    // TODO
+  },
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
+  },
+  destroyed () {
+    // TODO
+  },
+  activated () {
+    // TODO
+  },
+  deactivated () {
+    // TODO
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 30px;
+}
+</style>
