051aea2afbaeccd030ff3bbace4d0dcbd922e088
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-08 11:27:58 +0100

    Update AboutView.vue

--- mocaccino-community/mocaccino-community-frontend/src/views/AboutView.vue
+++ mocaccino-community/mocaccino-community-frontend/src/views/AboutView.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <welcomeComponent msg="about view"/>
+    <about msg="some text"/>
   </main>
 </template>
 
 <script>
 import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import AboutComponent from '@/components/AboutComponent.vue'
 
 export default {
   name: 'AboutView',
   components: {
-    welcomeComponent: WelcomeComponent
+    welcomeComponent: WelcomeComponent,
+    about: AboutComponent
   }
 }
 </script>
