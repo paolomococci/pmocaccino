a088009d0c274882be57c30ec4454ee1aa27c697
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-08 18:44:58 +0100

    Update HomeView.vue

--- mocaccino-community/mocaccino-community-frontend/src/views/HomeView.vue
+++ mocaccino-community/mocaccino-community-frontend/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <main>
-    <welcomeComponent msg="home view"/>
-    <home msg="some text"/>
+    <home msg=""/>
   </main>
 </template>
 
 <script>
-import WelcomeComponent from '@/components/WelcomeComponent.vue'
 import HomeComponent from '@/components/HomeComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    welcomeComponent: WelcomeComponent,
     home: HomeComponent
   }
 }
