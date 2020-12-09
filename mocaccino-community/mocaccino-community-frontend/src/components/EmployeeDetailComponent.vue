50f919687ea7cf47bd140780c1817bd903d9b002
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-09 10:53:20 +0100

    Update EmployeeDetailComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeDetailComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeDetailComponent.vue
@@ -1,15 +1,25 @@
 <template>
   <section>
-    <!-- TODO -->
+    <b-button variant="outline-info" @click="showModalDetail">open detail</b-button>
+    <b-modal ref="modal-detail" hide-footer title="detail of employee">
+      <div class="d-block text-center">
+        <h3>details</h3>
+      </div>
+      <b-button class="mt-3" variant="outline-info" block @click="hideModalDetail">close detail modal view</b-button>
+    </b-modal>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'EmployeeDetailComponent'
+  name: 'EmployeeDetailComponent',
+  methods: {
+    showModalDetail() {
+      this.$refs['modal-detail'].show()
+    },
+    hideModalDetail() {
+      this.$refs['modal-detail'].hide()
+    }
+  }
 }
 </script>
-
-<style lang="scss">
-
-</style>
