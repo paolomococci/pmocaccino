05eab589eb78ad6727a82a1398151c5cfedd0ea8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-13 20:07:24 +0100

    Update CompanyAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyAddComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <b-button variant="outline-primary" @click="showModalDetail">add company</b-button>
-    <b-modal ref="modal-add" hide-footer title="register a new company">
+    <b-modal ref="modal-add" hide-footer title="enter the details of a new company">
       <div class="d-block text-center">
         <h3>fields</h3>
       </div>
