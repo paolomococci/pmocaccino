a8811520269fe227192ff76c9a44ce286b7e24bb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-13 16:22:07 +0100

    Update EmployeeAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddComponent.vue
@@ -1,11 +1,25 @@
 <template>
   <section>
-    <!-- TODO -->
+    <b-button variant="outline-primary" @click="showModalDetail">add employee</b-button>
+    <b-modal ref="modal-add" hide-footer title="register a new employee">
+      <div class="d-block text-center">
+        <h3>fields</h3>
+      </div>
+      <b-button class="mt-3" variant="outline-info" block @click="hideModalDetail">cancel</b-button>
+    </b-modal>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'EmployeeAddComponent'
+  name: 'EmployeeAddComponent',
+  methods: {
+    showModalDetail() {
+      this.$refs['modal-add'].show()
+    },
+    hideModalDetail() {
+      this.$refs['modal-add'].hide()
+    }
+  }
 }
 </script>
