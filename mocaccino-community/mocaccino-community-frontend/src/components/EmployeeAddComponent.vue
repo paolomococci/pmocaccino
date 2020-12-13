2f8b86e66ab0187ce3d79ff82e1da98a157456a6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-13 23:47:49 +0100

    Update EmployeeAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddComponent.vue
@@ -4,8 +4,19 @@
     <b-modal ref="modal-add" hide-footer title="enter the details of a new employee">
       <div class="d-block text-center">
         <h3>fields</h3>
+        <b-form  @submit.stop.prevent>
+          <label for="feedback-username">username</label>
+          <b-form-input v-model="employeeUsername" :state="acceptable" id="feedback-username"></b-form-input>
+          <b-form-invalid-feedback :state="acceptable">
+            username of new company must be 6 to 14 characters long
+          </b-form-invalid-feedback>
+          <b-form-valid-feedback :state="acceptable">
+            all right
+          </b-form-valid-feedback>
+        </b-form>
       </div>
-      <b-button class="mt-3" variant="outline-info" block @click="hideModalDetail">cancel</b-button>
+      <b-button class="mt-3" variant="outline-secondary" block @click="hideModalDetail">cancel</b-button>
+      <b-button class="mt-3" variant="outline-primary" block @click="todoModalDetail">save</b-button>
     </b-modal>
   </section>
 </template>
@@ -13,12 +24,24 @@
 <script>
 export default {
   name: 'EmployeeAddComponent',
+  data: () => ({
+    url: 'http://localhost:8090/rest/employees',
+    employeeUsername: ''
+  }),
+  computed: {
+    acceptable() {
+      return this.employeeUsername.length > 5 && this.employeeUsername.length < 15;
+    }
+  },
   methods: {
     showModalDetail() {
-      this.$refs['modal-add'].show()
+      this.$refs['modal-add'].show();
     },
     hideModalDetail() {
-      this.$refs['modal-add'].hide()
+      this.$refs['modal-add'].hide();
+    },
+    todoModalDetail() {
+      this.$refs['modal-add'].hide();
     }
   }
 }
