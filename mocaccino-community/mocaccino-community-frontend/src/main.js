d5374b819ac1e04bdc6abfaa209b49b28cc0d678
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-14 19:13:23 +0100

    Update main.js

--- mocaccino-community/mocaccino-community-frontend/src/main.js
+++ mocaccino-community/mocaccino-community-frontend/src/main.js
@@ -5,10 +5,12 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { VuelidatePlugin } from '@vuelidate/core'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VuelidatePlugin)
 
 new Vue({
   router,
