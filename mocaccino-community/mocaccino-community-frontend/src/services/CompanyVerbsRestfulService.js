7495fa7f550f95e12fdf455056a69effd11c43e9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-15 22:58:29 +0100

    Update CompanyVerbsRestfulService.js

--- mocaccino-community/mocaccino-community-frontend/src/services/CompanyVerbsRestfulService.js
+++ mocaccino-community/mocaccino-community-frontend/src/services/CompanyVerbsRestfulService.js
@@ -1,8 +1,26 @@
+import axios from 'axios'
 import restful from '../../restful-config'
 
 class CompanyVerbsRestfulService {
+
+    read(uri) {
+        return axios.get(uri);
+    }
+
     readAll() {
         return restful.get('/companies');
+    }
+
+    update(uri, data) {
+        return axios.post(uri, data);
+    }
+
+    delete(uri) {
+        return axios.delete(uri);
+    }
+
+    searchByName(name) {
+        return restful.get(`/companies/search/likeByName?name=${name}`);
     }
 }
 
