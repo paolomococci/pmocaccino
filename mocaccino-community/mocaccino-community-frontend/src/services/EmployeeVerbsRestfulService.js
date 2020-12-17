be285951c13deacb7a0d240771d5d278c4865f66
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-17 10:56:44 +0100

    Update EmployeeVerbsRestfulService.js

--- mocaccino-community/mocaccino-community-frontend/src/services/EmployeeVerbsRestfulService.js
+++ mocaccino-community/mocaccino-community-frontend/src/services/EmployeeVerbsRestfulService.js
@@ -1,8 +1,42 @@
+import axios from 'axios'
 import restful from '../../restful-config'
 
 class EmployeeVerbsRestfulService {
+
+    create(data) {
+        return restful.post('/employees', data);
+    }
+
+    read(uri) {
+        return axios.get(uri);
+    }
+
     readAll() {
         return restful.get('/employees');
+    }
+
+    update(uri, data) {
+        return axios.post(uri, data);
+    }
+
+    delete(uri) {
+        return axios.delete(uri);
+    }
+
+    searchByName(name) {
+        return restful.get(`/employees/search/likeByName?name=${name}`);
+    }
+
+    searchBySurname(surname) {
+        return restful.get(`/employees/search/likeBySurname?surname=${surname}`);
+    }
+
+    searchByEmail(email) {
+        return restful.get(`/employees/search/likeByEmail?email=${email}`);
+    }
+
+    searchByUsername(username) {
+        return restful.get(`/employees/search/likeByUsername?username=${username}`);
     }
 }
 
