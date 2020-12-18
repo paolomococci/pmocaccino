fab72dc0368e69fd6291904e4765abe76d50585b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-18 07:17:02 +0100

    Update EmployeeVerbsRestfulService.js

--- mocaccino-community/mocaccino-community-frontend/src/services/EmployeeVerbsRestfulService.js
+++ mocaccino-community/mocaccino-community-frontend/src/services/EmployeeVerbsRestfulService.js
@@ -19,6 +19,10 @@ class EmployeeVerbsRestfulService {
         return axios.post(uri, data);
     }
 
+    partialUpdate(uri, data) {
+        return axios.patch(uri, data);
+    }
+
     delete(uri) {
         return axios.delete(uri);
     }
