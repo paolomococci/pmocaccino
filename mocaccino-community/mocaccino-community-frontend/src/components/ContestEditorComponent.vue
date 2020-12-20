8a39da5b7d7d03678ede9b6fc6888abc0786e2c9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-20 23:17:33 +0100

    Update ContestEditorComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/ContestEditorComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/ContestEditorComponent.vue
@@ -1,6 +1,25 @@
 <template>
   <section>
-      <!-- TODO -->
+    <b-button 
+      variant="outline-primary" 
+      @click="showModalDetail">edit</b-button>
+    <b-modal ref="modal-edit" hide-footer :title="uri">
+      <div class="d-block text-center">
+        <h3>fields</h3>
+        <!-- TODO -->
+      </div>
+      <b-button 
+        class="mt-3" 
+        variant="outline-secondary" 
+        block 
+        @click="hideModalDetail">cancel</b-button>
+      <b-button 
+        class="mt-3" 
+        variant="outline-primary" 
+        block 
+        :disabled="!validateForm"
+        @click="updateContest">save</b-button>
+    </b-modal>
   </section>
 </template>
 
@@ -13,7 +32,8 @@ export default {
     uri: String,
     nameField: String,
     titleField: String,
-    descriptionField: String
+    descriptionField: String,
+    dateField: Date
   },
   methods: {
     // TODO
