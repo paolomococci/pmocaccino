db8e3c7dfd9a08996399ddadd88c3cb4229aac56
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 20:59:16 +0100

    Update EmployeeEditorComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeEditorComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeEditorComponent.vue
@@ -6,7 +6,78 @@
     <b-modal ref="modal-edit" hide-footer :title="uri">
       <div class="d-block text-center">
         <h3>fields</h3>
-        <!-- TODO -->
+        <b-form  @submit.stop.prevent>
+          <!-- username field -->
+          <b-form-group>
+            <label for="feedback-username">username</label>
+            <b-form-input 
+              v-model="employeeUsername" 
+              :state="acceptableUsername" 
+              id="feedback-username"></b-form-input>
+            <b-form-invalid-feedback :state="acceptableUsername">
+              username of new employee must be 6 to 20 characters long
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback :state="acceptableUsername">
+              all right
+            </b-form-valid-feedback>
+          </b-form-group>
+          <!-- name field -->
+          <b-form-group>
+            <label for="feedback-name">name</label>
+            <b-form-input 
+              v-model="employeeName" 
+              :state="acceptableName" 
+              id="feedback-name"></b-form-input>
+            <b-form-invalid-feedback :state="acceptableName">
+              name of new employee must be 3 to 14 characters long
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback :state="acceptableName">
+              all right
+            </b-form-valid-feedback>
+          </b-form-group>
+          <!-- surname field -->
+          <b-form-group>
+            <label for="feedback-surname">surname</label>
+            <b-form-input 
+              v-model="employeeSurname" 
+              :state="acceptableSurname" 
+              id="feedback-surname"></b-form-input>
+            <b-form-invalid-feedback :state="acceptableSurname">
+              surname of new employee must be 3 to 20 characters long
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback :state="acceptableSurname">
+              all right
+            </b-form-valid-feedback>
+          </b-form-group>
+          <!-- email field -->
+          <b-form-group>
+            <label for="feedback-email">email</label>
+            <b-form-input 
+              v-model="employeeEmail" 
+              :state="acceptableEmail" 
+              id="feedback-email"></b-form-input>
+            <b-form-invalid-feedback :state="acceptableEmail">
+              email of new employee must be 8 to 40 characters long
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback :state="acceptableEmail">
+              all right
+            </b-form-valid-feedback>
+          </b-form-group>
+          <!-- profession field -->
+          <b-form-group>
+            <label for="feedback-profession">profession</label>
+            <b-form-input 
+              v-model="employeeProfession" 
+              :state="acceptableProfession" 
+              id="feedback-profession"></b-form-input>
+            <b-form-invalid-feedback :state="acceptableProfession">
+              profession of new employee must be 5 to 30 characters long
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback :state="acceptableProfession">
+              all right
+            </b-form-valid-feedback>
+          </b-form-group>
+        </b-form>
       </div>
       <b-button 
         class="mt-3" 
