eda4db3f3e63590ecac23dee8dcbbc9f9a0363f6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-21 20:36:19 +0100

    Update EmployeeEditorComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeEditorComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeEditorComponent.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import EmployeeVerbsRestfulService from '../services/EmployeeVerbsRestfulService'
+
 export default {
   name: 'EmployeeEditorComponent',
   data: () => ({
@@ -26,7 +28,26 @@ export default {
     // TODO
   },
   methods: {
-    // TODO
+    updateEmployee() {
+      var data = {
+        name: this.employeeName,
+        surname: this.employeeSurname,
+        email: this.employeeEmail,
+        profession: this.employeeProfession,
+        username: this.employeeUsername
+      };
+      EmployeeVerbsRestfulService.partialUpdate(this.uri, data)
+        .then(response => {
+          console.log(response.data);
+        }).catch(e => {
+          console.log(e);
+        });
+      this.$refs['modal-edit'].hide();
+      this.updateView();
+    },
+    updateView() {
+      this.$emit('updateView');
+    }
   },
   mounted() {
     this.employeeUsername = this.usernameField;
