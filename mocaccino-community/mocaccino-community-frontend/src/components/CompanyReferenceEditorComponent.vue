586031f8b70a8ccf3e012d54fdda453dddc4e1ce
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-22 17:29:08 +0100

    Update CompanyReferenceEditorComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyReferenceEditorComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyReferenceEditorComponent.vue
@@ -1,6 +1,28 @@
 <template>
     <section>
-        <!-- TODO -->
+      <b-button 
+        variant="outline-warning" 
+        size="sm" 
+        @click="showModalDetail">references</b-button>
+      <b-modal ref="modal-edit-reference" hide-footer :title="uri">
+        <div class="d-block text-center">
+          <h3>references</h3>
+          <b-form  @submit.stop.prevent>
+            <!-- TODO -->
+          </b-form>
+        </div>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">cancel</b-button>
+        <b-button 
+          class="mt-3" 
+          variant="outline-primary" 
+          block 
+          :disabled="!isAcceptable()" 
+          @click="hideModalDetail">update</b-button>
+      </b-modal>
     </section>
 </template>
 
@@ -13,7 +35,15 @@ export default {
     uri: String
   },
   methods: {
-    // TODO
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    }
   },
   mounted() {
     // TODO
