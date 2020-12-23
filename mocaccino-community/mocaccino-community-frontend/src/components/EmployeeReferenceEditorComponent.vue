b0a5ca1336283e3dd90cb805ba0467b933e87694
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-23 10:46:57 +0100

    Create EmployeeReferenceEditorComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeReferenceEditorComponent.vue
@@ -0,0 +1,82 @@
+<template>
+    <section>
+      <b-button 
+        variant="outline-warning" 
+        size="sm" 
+        @click="showModalDetail">references</b-button>
+      <b-modal ref="modal-edit-reference" hide-footer :title="uri">
+        <div class="d-block text-center">
+          <h3>references</h3>
+          <b-form  @submit.stop.prevent>
+            <!-- TODO -->
+          </b-form>
+        </div>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">cancel</b-button>
+        <b-button 
+          class="mt-3" 
+          variant="outline-primary" 
+          block 
+          :disabled="!isAcceptable()" 
+          @click="hideModalDetail">update</b-button>
+      </b-modal>
+    </section>
+</template>
+
+<script>
+import EmployeeVerbsRestfulService from '../services/EmployeeVerbsRestfulService'
+
+export default {
+    name: 'EmployeeReferenceEditorComponent',
+  data: () => ({
+    company: null,
+    contest: null
+  }),
+  props: {
+    uri: String
+  },
+  methods: {
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    },
+    retrieveCompany() {
+      EmployeeVerbsRestfulService.readCompanyMembership(this.uri)
+        .then(response => {
+          this.company = response.data._embedded.company;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    retrieveContest() {
+      EmployeeVerbsRestfulService.readContestParticipation(this.uri)
+        .then(response => {
+          this.contest = response.data._embedded.contest;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    settingUpCompanyMembership() {
+      // TODO
+    },
+    settingUpContestParticipation() {
+      // TODO
+    }
+  },
+  mounted() {
+    this.retrieveParticipants();
+  }
+}
+</script>
