6cf9a15cefcc0d2d2e69588707a615acfbda85dd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-24 09:12:09 +0100

    Update CompanyComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyComponent.vue
@@ -21,10 +21,6 @@
               <b-col sm="3" class="text-sm-right"><b>URI:</b></b-col>
               <b-col><output v-text="row.item._links.self.href"></output></b-col>
             </b-row>
-            <b-row class="mb-2">
-              <b-col sm="3" class="text-sm-right"><b>employees:</b></b-col>
-              <b-col><output v-text="row.item.employees"></output></b-col>
-            </b-row>
             <b-row>
               <edit-company 
                 :nameField="row.item.name" 
