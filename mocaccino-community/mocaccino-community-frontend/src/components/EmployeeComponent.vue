0c72d37f7af550998de1ee8fcaf825c414742e40
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-25 08:03:15 +0100

    Update EmployeeComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeComponent.vue
@@ -37,6 +37,11 @@
               <b-col sm="3" class="text-sm-right"><b>URI:</b></b-col>
               <b-col><output v-text="row.item._links.self.href"></output></b-col>
             </b-row>
+            <b-row class="mb-2">
+              <!-- TODO -->
+              <b-col sm="3" class="text-sm-right"><b>company:</b></b-col>
+              <b-col><output v-text="row.item._links.company.href"></output></b-col>
+            </b-row>
             <b-row>
               <edit-employee 
                 :usernameField="row.item.username" 
