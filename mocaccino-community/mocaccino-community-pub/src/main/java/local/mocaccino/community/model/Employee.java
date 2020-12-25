99197f2ffe32f19665f1d4b5ba4a50caf5987bf1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-25 19:19:40 +0100

    Update Employee.java

--- mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/model/Employee.java
+++ mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/model/Employee.java
@@ -22,6 +22,8 @@ import local.mocaccino.community.validator.constraint.AlphaConstraint;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -32,31 +34,42 @@ public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
 	private Long id;
 
 	@AlphaConstraint
+	@Size(min = 3, max = 14)
+	@Column(name = "NAME", columnDefinition = "VARCHAR(15)")
 	private String name;
 
 	@AlphaConstraint
+	@Size(min = 3, max = 20)
+	@Column(name = "SURNAME", columnDefinition = "VARCHAR(21)")
 	private String surname;
 
-	@AlphaConstraint
+	@Email
+	@Size(min = 8, max = 60)
+	@Column(name = "EMAIL", columnDefinition = "VARCHAR(61)")
 	private String email;
 
 	@AlphaConstraint
+	@Size(min = 5, max = 30)
+	@Column(name = "PROFESSION", columnDefinition = "VARCHAR(31)")
 	private String profession;
 
+	@Size(min = 6, max = 20)
+	@Column(name = "USERNAME", unique = true, nullable = false, columnDefinition = "VARCHAR(21)")
 	private String username;
 
 	@ManyToOne
-	@JoinColumn(name = "id_company")
+	@JoinColumn(name = "ID_COMPANY")
 	private Company company;
 
-	@ManyToMany(targetEntity = Contest.class, cascade = {CascadeType.ALL})
+	@ManyToMany(targetEntity = Contest.class, cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
 	@JoinTable(
-			name = "employee_contest",
-			joinColumns = {@JoinColumn(name = "id_employee")},
-			inverseJoinColumns = {@JoinColumn(name = "id_contest")}
+			name = "EMPLOYEE_CONTEST",
+			joinColumns = {@JoinColumn(name = "ID_EMPLOYEE")},
+			inverseJoinColumns = {@JoinColumn(name = "ID_CONTEST")}
 	)
 	private List<Contest> contests;
 }
