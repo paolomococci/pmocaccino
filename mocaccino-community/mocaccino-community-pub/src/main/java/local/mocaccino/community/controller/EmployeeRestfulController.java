3ea20297a25f273481fbc8b29ae1ae3508937649
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-26 09:27:53 +0100

    Create EmployeeRestfulController.java

---
+++ mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/controller/EmployeeRestfulController.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.mocaccino.community.controller;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URISyntaxException;
+
+@RepositoryRestController
+@RequestMapping(value = "/api/employees", produces = "application/hal+json")
+public class EmployeeRestfulController {
+
+    @PatchMapping(path = "/{id}/subscribe/contest")
+    public ResponseEntity<?> subscribeContest(@RequestBody String contestUri, @PathVariable String id)
+            throws URISyntaxException {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PatchMapping(path = "/{id}/debar/contest")
+    public ResponseEntity<?> debarContest(@RequestBody String contestUri, @PathVariable String id)
+            throws URISyntaxException {
+        // TODO
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+}
