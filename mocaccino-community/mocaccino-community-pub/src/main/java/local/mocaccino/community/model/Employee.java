a881acfd438ecce051a83be9867aeb56718fec9f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-26 20:52:10 +0100

    Update Employee.java

--- mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/model/Employee.java
+++ mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/model/Employee.java
@@ -19,14 +19,12 @@
 package local.mocaccino.community.model;
 
 import local.mocaccino.community.validator.constraint.AlphaConstraint;
-import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 import java.util.List;
 
-@Data
 @Entity
 @Table(name = "EMPLOYEE",
 		uniqueConstraints = @UniqueConstraint(columnNames = {"USERNAME"}))
@@ -72,4 +70,60 @@ public class Employee {
 			inverseJoinColumns = {@JoinColumn(name = "ID_CONTEST")}
 	)
 	private List<Contest> contests;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public List<Contest> getContests() {
+		return contests;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
 }
