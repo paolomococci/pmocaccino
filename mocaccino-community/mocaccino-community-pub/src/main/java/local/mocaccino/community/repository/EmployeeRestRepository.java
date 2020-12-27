c9c5f5d1d8cf90051a199e2f7837c1cc4ad4fede
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-27 10:15:14 +0100

    Update EmployeeRestRepository.java

--- mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/repository/EmployeeRestRepository.java
+++ mocaccino-community/mocaccino-community-pub/src/main/java/local/mocaccino/community/repository/EmployeeRestRepository.java
@@ -20,8 +20,11 @@ package local.mocaccino.community.repository;
 
 import local.mocaccino.community.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,24 +33,40 @@ import java.util.Optional;
 public interface EmployeeRestRepository
 		extends JpaRepository<Employee, Long> {
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE NAME LIKE ?1%")
-	List<Employee> likeByName(String name);
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE NAME LIKE :name")
+	List<Employee> likeByName(@Param("name") String name);
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE SURNAME LIKE ?1%")
-	List<Employee> likeBySurname(String surname);
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE SURNAME LIKE :surname")
+	List<Employee> likeBySurname(@Param("surname") String surname);
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE EMAIL LIKE ?1%")
-	List<Employee> likeByEmail(String email);
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE EMAIL LIKE :email")
+	List<Employee> likeByEmail(@Param("email") String email);
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE USERNAME LIKE ?1%")
-	List<Employee> likeByUsername(String username);
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE USERNAME LIKE :username")
+	List<Employee> likeByUsername(@Param("username") String username);
 
-	@Query(nativeQuery = true, value = "INSERT INTO EMPLOYEE_CONTEST VALUES(?1%,?2%)")
-	void subscribe(Long idEmployee, Long idContest);
+	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE USERNAME = :username")
+	Optional<Employee> findByUsername(@Param("username") String username);
 
-	@Query(nativeQuery = true, value = "DELETE FROM EMPLOYEE_CONTEST WHERE ID_EMPLOYEE=?1% AND ID_CONTEST=?2%")
-	void debar(Long idEmployee, Long idContest);
+	@Transactional
+	@Modifying
+	@Query(
+			nativeQuery = true,
+			value = "INSERT INTO EMPLOYEE_CONTEST VALUES(:idEmployee, :idContest)"
+	)
+	void subscribe(
+			@Param("idEmployee") Long idEmployee,
+			@Param("idContest") Long idContest
+	);
 
-	@Query(nativeQuery = true, value = "SELECT * FROM EMPLOYEE WHERE USERNAME=?1%")
-	Optional<Employee> findByUsername(String username);
+	@Transactional
+	@Modifying
+	@Query(
+			nativeQuery = true,
+			value = "DELETE FROM EMPLOYEE_CONTEST WHERE ID_EMPLOYEE = :idEmployee AND ID_CONTEST = :idContest"
+	)
+	void debar(
+			@Param("idEmployee") Long idEmployee,
+			@Param("idContest") Long idContest
+	);
 }
