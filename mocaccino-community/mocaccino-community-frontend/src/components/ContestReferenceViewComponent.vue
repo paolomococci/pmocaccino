b04ce094b913cca5233f95f409f467af650e5060
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-28 17:18:37 +0100

    Create ContestReferenceViewComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/ContestReferenceViewComponent.vue
@@ -0,0 +1,79 @@
+<template>
+    <section>
+      <b-button 
+        variant="outline-secondary" 
+        size="sm" 
+        @click="showModalDetail">view participants</b-button>
+      <b-modal ref="view-reference-participants" hide-footer :title="uri">
+        <div class="d-block text-center">
+          <h3>participants</h3>
+          <b-table 
+            :items="participants" 
+            :fields="fields" 
+            striped 
+            responsive="sm"></b-table>
+        </div>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">toggle</b-button>
+      </b-modal>
+    </section>
+</template>
+
+<script>
+import ContestVerbsRestfulService from '../services/ContestVerbsRestfulService'
+
+export default {
+    name: 'ContestReferenceViewComponent',
+  data: () => ({
+    fields: [
+      {
+        key: 'username',
+        sortable: true
+      },
+      {
+        key: 'name'
+      },
+      {
+        key: 'surname'
+      },
+      {
+        key: 'profession'
+      },
+      {
+        key: 'email'
+      }
+    ],
+    participants: []
+  }),
+  props: {
+    uri: String
+  },
+  methods: {
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['view-reference-participants'].show();
+    },
+    hideModalDetail() {
+      this.$refs['view-reference-participants'].hide();
+    },
+    retrieveListOfParticipants() {
+      ContestVerbsRestfulService.readListOfEmployee(this.uri)
+        .then(response => {
+          this.participants = response.data._embedded.employees;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    this.retrieveListOfParticipants();
+  }
+}
+</script>
