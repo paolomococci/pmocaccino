914db909a99c50507b1f75bfa321c4486eeb17d7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-28 17:21:14 +0100

    Create CompanyReferenceViewComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyReferenceViewComponent.vue
@@ -0,0 +1,79 @@
+<template>
+    <section>
+      <b-button 
+        variant="outline-secondary" 
+        size="sm" 
+        @click="showModalDetail">view staff</b-button>
+      <b-modal ref="modal-view-reference" hide-footer :title="uri">
+        <div class="d-block text-center">
+          <h3>staff</h3>
+          <b-table 
+            :items="staff" 
+            :fields="fields" 
+            striped 
+            responsive="sm"></b-table>
+        </div>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">toggle</b-button>
+      </b-modal>
+    </section>
+</template>
+
+<script>
+import CompanyVerbsRestfulService from '../services/CompanyVerbsRestfulService'
+
+export default {
+    name: 'CompanyReferenceViewComponent',
+  data: () => ({
+    fields: [
+      {
+        key: 'username',
+        sortable: true
+      },
+      {
+        key: 'name'
+      },
+      {
+        key: 'surname'
+      },
+      {
+        key: 'profession'
+      },
+      {
+        key: 'email'
+      }
+    ],
+    staff: []
+  }),
+  props: {
+    uri: String
+  },
+  methods: {
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['modal-view-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-view-reference'].hide();
+    },
+    retrieveStaffData() {
+      CompanyVerbsRestfulService.readListOfEmployee(this.uri)
+        .then(response => {
+          this.staff = response.data._embedded.employees;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    this.retrieveStaffData();
+  }
+}
+</script>
