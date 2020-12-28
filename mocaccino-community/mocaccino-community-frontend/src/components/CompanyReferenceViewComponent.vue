177f466e328ee2431ac56a5e6e0f24ce13991075
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-28 18:35:41 +0100

    Update CompanyReferenceViewComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyReferenceViewComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyReferenceViewComponent.vue
@@ -4,7 +4,7 @@
         variant="outline-secondary" 
         size="sm" 
         @click="showModalDetail">view staff</b-button>
-      <b-modal ref="modal-view-reference" hide-footer :title="uri">
+      <b-modal size="xl" scrollable ref="modal-view-reference" hide-footer :title="uri">
         <div class="d-block text-center">
           <h3>staff</h3>
           <b-table 
