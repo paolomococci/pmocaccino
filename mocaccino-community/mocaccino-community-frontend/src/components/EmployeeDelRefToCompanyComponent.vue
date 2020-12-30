5a76518ab76c9a4bd5269d0e2eb3c18e232e7632
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-30 11:57:13 +0100

    Create EmployeeDelRefToCompanyComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeDelRefToCompanyComponent.vue
@@ -0,0 +1,74 @@
+<template>
+    <section>
+      <b-button 
+        variant="outline-warning" 
+        @click="showModalDetail">references</b-button>
+      <b-modal ref="modal-edit-reference" hide-footer :title="uri">
+        <div class="d-block text-center">
+          <h3>references</h3>
+          <b-form  @submit.stop.prevent>
+          <!-- company reference -->
+          <b-form-group>
+            <label for="reference-company-name">company name</label>
+            <b-form-input 
+              readonly
+              v-model="company.name" 
+              id="reference-company-name"></b-form-input>
+          </b-form-group>
+          </b-form>
+        </div>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">toggle</b-button>
+        <b-button 
+          class="mt-3" 
+          variant="outline-primary" 
+          block 
+          @click="ruleOutCompanyMembership">delete</b-button>
+      </b-modal>
+    </section>
+</template>
+
+<script>
+import EmployeeVerbsRestfulService from '../services/EmployeeVerbsRestfulService'
+
+export default {
+    name: 'EmployeeDelRefToCompanyComponent',
+  data: () => ({
+    company: ''
+  }),
+  props: {
+    uri: String
+  },
+  methods: {
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    },
+    retrieveCompany() {
+      EmployeeVerbsRestfulService.readCompanyMembership(this.uri)
+        .then(response => {
+          this.company = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    ruleOutCompanyMembership() {
+      // TODO
+      this.hideModalDetail();
+    }
+  },
+  mounted() {
+    this.retrieveCompany();
+  }
+}
+</script>
