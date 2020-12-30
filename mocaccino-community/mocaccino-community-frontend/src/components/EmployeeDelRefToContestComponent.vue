95bbcb8fbfafc7d8f7f756a29e44b390bd1278f1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-30 11:59:16 +0100

    Create EmployeeDelRefToContestComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeDelRefToContestComponent.vue
@@ -0,0 +1,74 @@
+<template>
+    <section>
+      <b-button 
+        variant="outline-warning" 
+        @click="showModalDetail">references</b-button>
+      <b-modal ref="modal-edit-reference" hide-footer :title="uri">
+        <div class="d-block text-center">
+          <h3>references</h3>
+          <b-form  @submit.stop.prevent>
+          <!-- contest reference -->
+          <b-form-group>
+            <label for="reference-contest-name">contest name</label>
+            <b-form-input 
+              readonly
+              v-model="contest.name" 
+              id="reference-contest-name"></b-form-input>
+          </b-form-group>
+          </b-form>
+        </div>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">toggle</b-button>
+        <b-button 
+          class="mt-3" 
+          variant="outline-primary" 
+          block 
+          @click="ruleOutContestParticipation">delete</b-button>
+      </b-modal>
+    </section>
+</template>
+
+<script>
+import EmployeeVerbsRestfulService from '../services/EmployeeVerbsRestfulService'
+
+export default {
+    name: 'EmployeeDelRefToContestComponent',
+  data: () => ({
+    contests: []
+  }),
+  props: {
+    uri: String
+  },
+  methods: {
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    },
+    retrieveContests() {
+      EmployeeVerbsRestfulService.readContestParticipation(this.uri)
+        .then(response => {
+          this.contests = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    ruleOutContestParticipation() {
+      // TODO
+      this.hideModalDetail();
+    }
+  },
+  mounted() {
+    this.retrieveContests();
+  }
+}
+</script>
