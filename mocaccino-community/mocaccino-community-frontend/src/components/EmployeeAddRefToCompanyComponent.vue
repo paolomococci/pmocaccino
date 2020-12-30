5446c49594b7de0108cacbc6b2345cab84c852cb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-30 20:14:48 +0100

    Update EmployeeAddRefToCompanyComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddRefToCompanyComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddRefToCompanyComponent.vue
@@ -11,7 +11,7 @@
           <b-form-group>
             <label>company name</label>
             <!-- TODO -->
-            <b-form-select></b-form-select>
+            <b-form-select v-model="company" :options="companies"></b-form-select>
           </b-form-group>
           </b-form>
         </div>
