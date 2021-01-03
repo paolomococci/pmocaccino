7cf2206d91dd8952ab87dbc75ff845b74907b4a7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-03 12:30:25 +0100

    Update EmployeeAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeAddComponent.vue
@@ -98,6 +98,8 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, minLength, maxLength, helpers } from "vuelidate/lib/validators";
 import EmployeeVerbsRestfulService from '../services/EmployeeVerbsRestfulService'
 
 export default {
@@ -109,29 +111,8 @@ export default {
     employeeEmail: '',
     employeeProfession: ''
   }),
-  computed: {
-    acceptableUsername() {
-      return this.isAcceptableUsername();
-    },
-    acceptableName() {
-      return this.isAcceptableName();
-    },
-    acceptableSurname() {
-      return this.isAcceptableSurname();
-    },
-    acceptableEmail() {
-      return this.isAcceptableEmail();
-    },
-    acceptableProfession() {
-      return this.isAcceptableProfession();
-    },
-    validateForm() {
-      return this.acceptableUsername && 
-        this.acceptableName && 
-        this.acceptableSurname && 
-        this.acceptableEmail && 
-        this.acceptableProfession;
-    }
+  validations: {
+    // TODO
   },
   methods: {
     isAcceptableUsername() {
