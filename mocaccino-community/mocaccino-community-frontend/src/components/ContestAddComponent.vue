faec22a2457c2f2e6abb55c73efd064bbd640c0c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-03 19:22:23 +0100

    Update ContestAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/ContestAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/ContestAddComponent.vue
@@ -78,6 +78,18 @@ export default {
   validations: {
   },
   methods: {
+    onValidateForm(companyName) {
+      // TODO
+    },
+    onResetForm() {
+      // TODO
+    },
+    onSubmitForm() {
+      // TODO
+    },
+    onConfirm() {
+      // TODO
+    },
     showModalDetail() {
       this.$refs['modal-add'].show();
     },
