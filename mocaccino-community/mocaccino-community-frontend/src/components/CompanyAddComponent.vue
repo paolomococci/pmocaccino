91688d22e7ad642268265c4c4026a1430294c8e6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-03 12:26:39 +0100

    Update CompanyAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyAddComponent.vue
@@ -9,53 +9,107 @@
     <b-modal ref="modal-add" hide-footer title="enter the details of a new company">
       <div class="d-block text-center">
         <h3>fields</h3>
-        <b-form  @submit.stop.prevent>
+        <b-form  @submit.stop.prevent="onSubmitForm">
           <!-- name field -->
-          <label for="feedback-name">name</label>
-          <b-form-input 
-            required
-            v-model="companyName" 
-            :state="acceptableName" 
-            id="feedback-name"></b-form-input>
-          <b-form-invalid-feedback :state="acceptableName">
-            name of new company must be 8 to 30 characters long
-          </b-form-invalid-feedback>
-          <b-form-valid-feedback :state="acceptableName">
-            all right
-          </b-form-valid-feedback>
+          <b-form-group id="input-name-group" label="name" label-for="input-name">
+            <b-form-input 
+              id="input-name"
+              name="input-name"
+              v-model="$v.form.companyName.$model" 
+              :state="onValidateForm('companyName')" 
+              aria-describedby="input-name-feedback-invalid"></b-form-input>
+            <b-form-invalid-feedback id="input-name-feedback-invalid">
+              must be 8 to 30 characters long, moreover the symbols and white space are prohibited
+            </b-form-invalid-feedback>
+            <b-form-valid-feedback>
+              all right
+            </b-form-valid-feedback>
+          </b-form-group>
+        <b-button 
+          class="mt-3" 
+          variant="outline-secondary" 
+          block 
+          @click="hideModalDetail">cancel</b-button>
+        <b-button 
+          class="mt-3" 
+          variant="outline-warning" 
+          block 
+          @click="onResetForm()">reset</b-button>
+        <b-button 
+          class="mt-3" 
+          variant="outline-primary" 
+          block 
+          type="submit">save</b-button>
         </b-form>
       </div>
-      <b-button 
-        class="mt-3" 
-        variant="outline-secondary" 
-        block 
-        @click="hideModalDetail">cancel</b-button>
-      <b-button 
-        class="mt-3" 
-        variant="outline-primary" 
-        block 
-        :disabled="!isAcceptableName()" 
-        @click="addCompany">save</b-button>
     </b-modal>
   </section>
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, minLength, maxLength, helpers } from "vuelidate/lib/validators";
 import CompanyVerbsRestfulService from '../services/CompanyVerbsRestfulService'
+
+const charactersAcceptedAsValid = helpers.regex('charactersAcceptedAsValid', /^[a-zA-Z0-9-_]*$/);
 
 export default {
   name: 'CompanyAddComponent',
+  mixins: [validationMixin],
   data: () => ({
+    form: {
+      companyName: ''
+    },
     companyName: ''
   }),
-  computed: {
-    acceptableName() {
-      return this.isAcceptableName();
+  validations: {
+    form: {
+      companyName: {
+        required,
+        minLength: minLength(8),
+        maxLength: maxLength(30),
+        charactersAcceptedAsValid
+      }
     }
   },
   methods: {
-    isAcceptableName() {
-      return this.companyName.length > 7 && this.companyName.length < 31;
+    onValidateForm(companyName) {
+      const { $dirty, $error } = this.$v.form[companyName];
+      return $dirty ? !$error : null;
+    },
+    onResetForm() {
+      this.form = {
+        companyName: ''
+      };
+      this.$nextTick(() => {
+        this.$v.$reset();
+      });
+    },
+    onSubmitForm() {
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        this.onResetForm();
+        return;
+      }
+      this.onConfirm();
+    },
+    onConfirm() {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to add this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'warning',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.addCompany();
+        }
+      });
     },
     showModalDetail() {
       this.$refs['modal-add'].show();
@@ -65,7 +119,7 @@ export default {
     },
     addCompany() {
       var data = {
-        name: this.companyName
+        name: this.form.companyName
       };
       CompanyVerbsRestfulService.create(data)
         .then(response => {
