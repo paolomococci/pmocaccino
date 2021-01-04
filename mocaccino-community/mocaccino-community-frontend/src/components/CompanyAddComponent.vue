da4da8c800beaf7333999345a3925727921765ce
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-04 10:53:05 +0100

    Update CompanyAddComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyAddComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyAddComponent.vue
@@ -25,6 +25,7 @@
               all right
             </b-form-valid-feedback>
           </b-form-group>
+          <!-- buttons -->
           <b-button 
             class="mt-3" 
             variant="outline-secondary" 
