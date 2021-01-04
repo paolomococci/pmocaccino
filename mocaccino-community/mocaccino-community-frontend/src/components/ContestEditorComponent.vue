1154966240cce60a648af5f14d64fc3484d6f243
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-04 21:34:54 +0100

    Update ContestEditorComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/ContestEditorComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/ContestEditorComponent.vue
@@ -163,7 +163,12 @@ export default {
       });
     },
     onSubmitForm() {
-      // TODO
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        this.onResetForm();
+        return;
+      }
+      this.onConfirm();
     },
     onConfirm() {
       this.messageBoxToConfirmDeletion = '';
