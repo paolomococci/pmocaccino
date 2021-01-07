f5924b114c0257554586ae5113d8a7b059cdeed3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-07 21:56:57 +0100

    Create CompanyViewTableComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyViewTableComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <!-- TODO-->
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'CompanyViewTableComponent',
+  data: () => ({}),
+  methods: {},
+  computed: {},
+  mounted() {}
+}
+</script>
