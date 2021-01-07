b37d8d3147b9d3b207ab8300adf5c4f847dda6ca
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-07 21:56:04 +0100

    Create EmployeeViewTableComponent.vue

---
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeViewTableComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <!-- TODO-->
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'EmployeeViewTableComponent',
+  data: () => ({}),
+  methods: {},
+  computed: {},
+  mounted() {}
+}
+</script>
