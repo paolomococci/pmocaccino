669039a05db3323d14433f2eae1beaa6b37b86e2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-08 05:00:21 +0100

    Update CompanyViewTableComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanyViewTableComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanyViewTableComponent.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import CompanyVerbsRestfulService from '../services/CompanyVerbsRestfulService'
+
 export default {
     name: 'CompanyViewTableComponent',
   data: () => ({
@@ -23,7 +25,18 @@ export default {
     messageBoxToConfirmDeletion: '',
     textToSearchFor: ''
   }),
-  methods: {},
+  methods: {
+    retrieveCompanies() {
+      CompanyVerbsRestfulService.readAll()
+        .then(response => {
+          this.companies = response.data._embedded.companies;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
   computed: {},
   mounted() {}
 }
