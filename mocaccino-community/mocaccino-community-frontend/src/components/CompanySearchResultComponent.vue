2280516c2eb79f52568199121f813fd1518c421c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-08 04:36:46 +0100

    Update CompanySearchResultComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/CompanySearchResultComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/CompanySearchResultComponent.vue
@@ -5,10 +5,23 @@
 </template>
 
 <script>
+import CompanyVerbsRestfulService from '../services/CompanyVerbsRestfulService'
+
 export default {
     name: 'CompanySearchResultComponent',
   data: () => ({}),
-  methods: {},
+  methods: {
+    searchByName() {
+      CompanyVerbsRestfulService.searchByName(this.textToSearchFor)
+        .then(response => {
+          this.companies = response.data._embedded.companies;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
   computed: {},
   mounted() {}
 }
