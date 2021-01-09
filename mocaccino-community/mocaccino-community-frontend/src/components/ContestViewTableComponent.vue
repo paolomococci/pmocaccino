8a60e5d1c6b777270a3fc4c24f62ba75bd41a60f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-09 06:58:07 +0100

    Update ContestViewTableComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/ContestViewTableComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/ContestViewTableComponent.vue
@@ -26,8 +26,7 @@ export default {
       }
     ],
     contests: [],
-    messageBoxToConfirmDeletion: '',
-    textToSearchFor: ''
+    messageBoxToConfirmDeletion: ''
   }),
   methods: {
     retrieveContests() {
