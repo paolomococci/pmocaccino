aac5bd3bc72f6b037d6b625a4df610dcdec06376
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-09 06:58:31 +0100

    Update EmployeeViewTableComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeViewTableComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeViewTableComponent.vue
@@ -22,8 +22,7 @@ export default {
       }
     ],
     employees: [],
-    messageBoxToConfirmDeletion: '',
-    textToSearchFor: ''
+    messageBoxToConfirmDeletion: ''
   }),
   methods: {
     retrieveEmployees() {
