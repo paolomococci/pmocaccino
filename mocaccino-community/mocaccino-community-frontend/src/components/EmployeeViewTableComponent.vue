19477bce858fbbab01f873a75c44cd1a4d029d5c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-09 06:48:38 +0100

    Update EmployeeViewTableComponent.vue

--- mocaccino-community/mocaccino-community-frontend/src/components/EmployeeViewTableComponent.vue
+++ mocaccino-community/mocaccino-community-frontend/src/components/EmployeeViewTableComponent.vue
@@ -49,6 +49,31 @@ export default {
           this.employees = null;
           this.retrieveEmployees();
         }, 2000);
+    },
+    deleteItem(uri) {
+      EmployeeVerbsRestfulService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
+    },
+    deleteItemConfirm(uri) {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.deleteItem(uri);
+        }
+      });
     }
   },
   computed: {},
