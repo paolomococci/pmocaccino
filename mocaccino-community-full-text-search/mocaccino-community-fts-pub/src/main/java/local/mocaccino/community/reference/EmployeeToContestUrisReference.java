6d832d6c8b523ff3fb87598dd65551f7b51e0a01
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-11 10:34:25 +0100

    Create EmployeeToContestUrisReference.java

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-pub/src/main/java/local/mocaccino/community/reference/EmployeeToContestUrisReference.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright 2020 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed following in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.mocaccino.community.reference;
+
+import java.net.URI;
+
+public class EmployeeToContestUrisReference {
+
+    private URI employeeUri;
+    private URI contestUri;
+
+    public URI getEmployeeUri() {
+        return employeeUri;
+    }
+
+    public void setEmployeeUri(URI employeeUri) {
+        this.employeeUri = employeeUri;
+    }
+
+    public URI getContestUri() {
+        return contestUri;
+    }
+
+    public void setContestUri(URI contestUri) {
+        this.contestUri = contestUri;
+    }
+}
