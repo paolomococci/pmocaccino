1cf18b0f34991432c964c753df7f8acf639e8530
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-12 20:19:02 +0100

    Create HomeView.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <welcome msg="home view"/>
+  </section>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    welcome: WelcomeComponent
+  }
+}
+</script>
