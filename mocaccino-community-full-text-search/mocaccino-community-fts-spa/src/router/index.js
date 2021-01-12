cad17e34e13bf7715648519939146c1e031f3028
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-12 20:17:18 +0100

    Create index.js

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomeView',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'AboutView',
+    component: () => import('../views/AboutView.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
