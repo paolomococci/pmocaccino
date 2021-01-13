8f15107bb0deced118ea8fb355e0341313f91c8a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-13 20:01:52 +0100

    Create HomeComponent.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/HomeComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 v-text="msg"></h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
