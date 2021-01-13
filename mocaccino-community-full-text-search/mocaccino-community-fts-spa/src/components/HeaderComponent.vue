4eeac462bca32f0c35a52ac2f8a5fb83d7e96def
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-13 20:00:30 +0100

    Create HeaderComponent.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 v-text="msg"></h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
