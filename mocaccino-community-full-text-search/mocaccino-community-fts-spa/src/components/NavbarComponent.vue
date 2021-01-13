ec7619a48ed50fa41601db0640b8d0c601007145
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-13 20:00:55 +0100

    Create NavbarComponent.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 v-text="msg"></h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
