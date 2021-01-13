e146584979771ffe1567db6e458ff6c32e9edf7b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-13 19:43:08 +0100

    Create CompanyView.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/views/CompanyView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <welcome msg="home view"/>
+    <!-- TODO -->
+  </main>
+</template>
+
+<script>
+import WelcomeComponent from '@/components/WelcomeComponent.vue'
+
+export default {
+  name: 'CompanyView',
+  components: {
+    welcome: WelcomeComponent
+  },
+  mounted () {
+    // TODO
+  },
+  destroyed () {
+    // TODO
+  }
+}
+</script>
