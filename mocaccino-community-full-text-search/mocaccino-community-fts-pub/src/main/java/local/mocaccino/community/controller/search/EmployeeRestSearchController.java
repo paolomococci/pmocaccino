570969348d6738a14b89692d3f37ba3b50874ace
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-13 18:45:52 +0100

    Update EmployeeRestSearchController.java

--- mocaccino-community-full-text-search/mocaccino-community-fts-pub/src/main/java/local/mocaccino/community/controller/search/EmployeeRestSearchController.java
+++ mocaccino-community-full-text-search/mocaccino-community-fts-pub/src/main/java/local/mocaccino/community/controller/search/EmployeeRestSearchController.java
@@ -18,6 +18,7 @@
 
 package local.mocaccino.community.controller.search;
 
+import local.mocaccino.community.model.Employee;
 import local.mocaccino.community.service.EmployeeHibernateSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,6 +29,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/employee/search")
 public class EmployeeRestSearchController {
@@ -37,8 +40,13 @@ public class EmployeeRestSearchController {
 
     @GetMapping
     public ResponseEntity<?> search(
-            @RequestParam(value = "search", required = false) String query, Model model
+            @RequestParam(value = "query", required = false) String query, Model model
     ) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        List<Employee> employees = employeeHibernateSearchService.employeeUsernameFuzzySearch(query);
+        if (employees != null) {
+            // TODO
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
