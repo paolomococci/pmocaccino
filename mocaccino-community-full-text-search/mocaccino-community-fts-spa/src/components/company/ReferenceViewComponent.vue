3b9b35d813e1808c29087c969da88c3e03cda0dc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-14 17:45:09 +0100

    Create ReferenceViewComponent.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/company/ReferenceViewComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h1>{{msg}}</h1>
+  </section>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 20px;
+  font-weight: lighter;
+  margin: 40px;
+}
+</style>
+
+<script>
+
+export default {
+  name: 'ReferenceViewComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
