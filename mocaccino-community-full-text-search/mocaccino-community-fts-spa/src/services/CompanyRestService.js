df1a495cebd2923607a976795e98386b263b324f
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-14 17:50:50 +0100

    Update CompanyRestService.js

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/services/CompanyRestService.js
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/services/CompanyRestService.js
@@ -1,1 +1,39 @@
+import axios from 'axios'
+import rest from '../../rest-config'
 
+class CompanyRestService {
+
+    create(data) {
+        return rest.post('rest/companies', data);
+    }
+
+    read(uri) {
+        return axios.get(uri);
+    }
+
+    readAll() {
+        return rest.get('rest/companies');
+    }
+
+    update(uri, data) {
+        return axios.put(uri, data);
+    }
+
+    partialUpdate(uri, data) {
+        return axios.patch(uri, data);
+    }
+
+    delete(uri) {
+        return axios.delete(uri);
+    }
+
+    searchByName(name) {
+        return rest.get(`rest/companies/search/likeByName?name=${name}`);
+    }
+
+    readListOfEmployee(uri) {
+        return rest.get(`${uri}/employees`);
+    }
+}
+
+export default new CompanyRestService();
