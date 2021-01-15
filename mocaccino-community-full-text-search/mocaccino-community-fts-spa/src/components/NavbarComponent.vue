a18d40b944e0392dcb2e0c191fd240e737e3faa4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-15 15:02:24 +0100

    Update NavbarComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
@@ -1,5 +1,17 @@
 <template>
-  <section>
+  <section class="centerx">
+    <vs-navbar>
+      <div slot="title">
+        <vs-navbar-title>
+          mocaccino community
+        </vs-navbar-title>
+      </div>
+      <vs-navbar-item index="0"><router-link to="/">home</router-link></vs-navbar-item>
+      <vs-navbar-item index="1"><router-link to="/about">about</router-link></vs-navbar-item>
+      <vs-navbar-item index="2"><router-link to="/company">company</router-link></vs-navbar-item>
+      <vs-navbar-item index="3"><router-link to="/contest">contest</router-link></vs-navbar-item>
+      <vs-navbar-item index="4"><router-link to="/employee">employee</router-link></vs-navbar-item>
+    </vs-navbar>
     <h1 v-text="msg"></h1>
   </section>
 </template>
