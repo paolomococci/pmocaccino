52f2a75b9fa1e5baaa26535caaf4409cf69f1a39
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-16 20:49:12 +0100

    Update main.js

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/main.js
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/main.js
@@ -2,13 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuesax from 'vuesax'
-import 'vuesax/dist/vuesax.css'
-import 'material-icons/iconfont/material-icons.css';
 
 Vue.config.productionTip = false
-
-Vue.use(Vuesax)
 
 new Vue({
   router,
