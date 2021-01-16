b0e038a536200939b848583def7d921adefccf8b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-16 21:40:37 +0100

    Update App.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/App.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/App.vue
@@ -16,23 +16,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #235;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #235;
-    &.router-link-exact-active {
-      color: #4b8;
-    }
-  }
-}
-</style>
