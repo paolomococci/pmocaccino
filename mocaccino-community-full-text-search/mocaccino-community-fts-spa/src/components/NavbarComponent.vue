f47d6bfae01f82fd89fe7d09fe2c4635393871f8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-16 21:42:21 +0100

    Update NavbarComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
@@ -1,7 +1,35 @@
 <template>
   <section>
     <!-- TODO-->
-    <h1 v-text="msg"></h1>
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{path: '/'}">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>mocaccino community</strong>
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item tag="router-link" :to="{path: '/about'}">
+          about
+        </b-navbar-item>
+      </template>
+      <template #end>
+      <b-navbar-dropdown label="data">
+        <b-navbar-item tag="router-link" :to="{path: '/company'}">
+          company
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{path: '/contest'}">
+          contest
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{path: '/employee'}">
+          employee
+        </b-navbar-item>
+      </b-navbar-dropdown>
+      </template>
+    </b-navbar>
   </section>
 </template>
 
