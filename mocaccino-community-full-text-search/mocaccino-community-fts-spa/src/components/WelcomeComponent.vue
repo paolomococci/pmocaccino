9adadb8fcaa03f0542b9dd550a23f5bee77ffcf6
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-16 20:50:12 +0100

    Update WelcomeComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/WelcomeComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/WelcomeComponent.vue
@@ -1,17 +1,6 @@
 <template>
   <section>
-    <vs-row vs-justify="center">
-      <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-        <vs-card>
-          <section slot="header">
-            <h3>mocaccino</h3>
-          </section>
-          <section>
-            <span v-text="msg"></span>
-          </section>
-        </vs-card>
-      </vs-col>
-    </vs-row>
+    <!-- TODO-->>
   </section>
 </template>
 
