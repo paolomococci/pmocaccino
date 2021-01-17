92970cb4498abc0e15865baf9fc46cab19586727
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-17 17:42:16 +0100

    Update AboutView.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/views/AboutView.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/views/AboutView.vue
@@ -1,16 +1,16 @@
 <template>
-  <section>
-    <welcome msg="about view"/>
-  </section>
+  <main>
+    <about-component msg="In this demo you have the possibility to modify the data by calling a RESTful API through a convenient graphical interface."/>
+  </main>
 </template>
 
 <script>
-import WelcomeComponent from '@/components/WelcomeComponent.vue'
+import AboutComponent from '@/components/AboutComponent.vue'
 
 export default {
   name: 'AboutView',
   components: {
-    welcome: WelcomeComponent
+    'about-component': AboutComponent
   }
 }
 </script>
