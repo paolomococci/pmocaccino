615625de80bf3d0697609b3df9857045c3a03edb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-17 11:14:36 +0100

    Update NavbarComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/NavbarComponent.vue
@@ -2,30 +2,42 @@
   <section>
     <!-- TODO-->
     <b-navbar>
-      <template #brand>
-        <b-navbar-item tag="router-link" :to="{path: '/'}">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>mocaccino community</strong>
-            </a>
-          </div>
-        </b-navbar-item>
-      </template>
       <template #start>
+        <b-navbar-item tag="router-link" :to="{path: '/'}">
+          <b-tooltip label="mocaccino community" position="is-right">
+            <div class="buttons">
+              <a class="button is-primary">
+                <font-awesome-icon :icon="coffeeIcon" size="lg"/>
+              </a>
+            </div>
+          </b-tooltip>
+        </b-navbar-item>
         <b-navbar-item tag="router-link" :to="{path: '/about'}">
-          about
+          <b-tooltip label="about" position="is-right">
+            <div class="buttons">
+              <a class="button is-primary">
+                <font-awesome-icon :icon="aboutIcon" size="lg"/>
+              </a>
+            </div>
+          </b-tooltip>
         </b-navbar-item>
       </template>
       <template #end>
-      <b-navbar-dropdown label="data">
+      <b-navbar-dropdown label="manage">
         <b-navbar-item tag="router-link" :to="{path: '/company'}">
-          company
+          <b-tooltip label="company" position="is-left">
+            <font-awesome-icon :icon="companyIcon"/>
+          </b-tooltip>
         </b-navbar-item>
         <b-navbar-item tag="router-link" :to="{path: '/contest'}">
-          contest
+          <b-tooltip label="contest" position="is-left">
+            <font-awesome-icon :icon="contestIcon"/>
+          </b-tooltip>
         </b-navbar-item>
         <b-navbar-item tag="router-link" :to="{path: '/employee'}">
-          employee
+          <b-tooltip label="employee" position="is-left">
+            <font-awesome-icon :icon="employeeIcon"/>
+          </b-tooltip>
         </b-navbar-item>
       </b-navbar-dropdown>
       </template>
@@ -34,10 +46,37 @@
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faCoffee,
+  faBroadcastTower,
+  faBuilding,
+  faUsers,
+  faUser } from '@fortawesome/free-solid-svg-icons'
+
 export default {
   name: 'NavbarComponent',
+  components: {
+    FontAwesomeIcon
+  },
   props: {
     msg: String
+  },
+  data: () => ({
+    coffeeIcon: faCoffee,
+    aboutIcon: faBroadcastTower,
+    companyIcon: faBuilding,
+    contestIcon: faUsers,
+    employeeIcon: faUser
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
