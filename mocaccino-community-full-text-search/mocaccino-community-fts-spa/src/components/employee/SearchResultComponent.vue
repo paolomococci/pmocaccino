9fe0d84558617d9f3bcb7fab6e401d34ec984684
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-17 11:53:06 +0100

    Update SearchResultComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/SearchResultComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/SearchResultComponent.vue
@@ -9,6 +9,18 @@ export default {
   name: 'SearchResultComponent',
   props: {
     msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
   }
 }
 </script>
