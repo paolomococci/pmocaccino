539cd2d033fb667281d325eac5a77b0965adb052
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-17 17:44:29 +0100

    Create AboutComponent.vue

---
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/AboutComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4" v-text="msg"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'AboutComponent',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    // TODO
+  }),
+  methods: {
+    // TODO
+  },
+  computed: {
+    // TODO
+  },
+  mounted() {
+    // TODO
+  }
+}
+</script>
