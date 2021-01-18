4875af8b9fc98b161c0402baa30887067c38965b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-18 10:55:21 +0100

    Update ViewTableComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/company/ViewTableComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/company/ViewTableComponent.vue
@@ -5,22 +5,82 @@
 </template>
 
 <script>
+import CompanyRestService from '../services/CompanyRestService'
+
 export default {
   name: 'ViewTableComponent',
   props: {
     msg: String
   },
   data: () => ({
-    // TODO
+    fields: [
+      {
+        key: 'name',
+        label: 'names',
+        sortable: true
+      },
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ],
+    companies: [],
+    messageBoxToConfirmDeletion: ''
   }),
   methods: {
-    // TODO
+    retrieveCompanies() {
+      CompanyRestService.readAll()
+        .then(response => {
+          this.companies = response.data._embedded.companies;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    async updateView() {
+      this.$bvToast.toast('data of companies being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.companies = null;
+          this.retrieveCompanies();
+        }, 1000);
+    },
+    deleteItem(uri) {
+      CompanyRestService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
+    },
+    deleteItemConfirm(uri) {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.deleteItem(uri);
+        }
+      });
+    }
   },
-  computed: {
-    // TODO
-  },
+  computed: {},
   mounted() {
-    // TODO
+    this.retrieveCompanies();
   }
 }
 </script>
