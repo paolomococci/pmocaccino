386e7cafd00d1b075c3ad1562ea69c76251146a7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-18 10:53:53 +0100

    Update EditorComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/company/EditorComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/company/EditorComponent.vue
@@ -1,26 +1,113 @@
 <template>
   <section>
-    <h1 v-text="msg"></h1>
+    <!-- todo -->
   </section>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import {
+  required,
+  minLength,
+  maxLength,
+  helpers
+} from 'vuelidate/lib/validators'
+import CompanyRestService from '../services/CompanyRestService'
+
+const charactersAcceptedAsValid = helpers
+  .regex('charactersAcceptedAsValid', /^[a-zA-Z0-9-_]*$/);
+
 export default {
   name: 'EditorComponent',
-  props: {
-    msg: String
-  },
+  mixins: [validationMixin],
   data: () => ({
-    // TODO
+    form: {
+      companyName: ''
+    }
   }),
+  props: {
+    uri: String,
+    nameField: String
+  },
+  validations: {
+    form: {
+      companyName: {
+        required,
+        minLength: minLength(8),
+        maxLength: maxLength(30),
+        charactersAcceptedAsValid
+      }
+    }
+  },
   methods: {
-    // TODO
+    onValidateFormCompanyName(companyName) {
+      const { $dirty, $error } = this.$v.form[companyName];
+      return $dirty ? !$error : null;
+    },
+    onResetForm() {
+      this.form = {
+        companyName: ''
+      };
+      this.$nextTick(() => {
+        this.$v.$reset();
+      });
+    },
+    onSubmitForm() {
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        this.onResetForm();
+        return;
+      }
+      this.onConfirm();
+    },
+    onConfirm() {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to update this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'warning',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.updateCompany();
+        }
+      });
+    },
+    showModalDetail() {
+      this.$refs['modal-edit'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit'].hide();
+    },
+    updateCompany() {
+      var data = {
+        name: this.form.companyName
+      };
+      CompanyRestService.partialUpdate(this.uri, data)
+        .then(response => {
+          console.log(response.data);
+        }).catch(e => {
+          console.log(e);
+        });
+      this.$refs['modal-edit'].hide();
+      this.updateView();
+    },
+    updateView() {
+      this.$emit('updateView');
+    }
   },
   computed: {
-    // TODO
+    acceptable() {
+      return this.isAcceptable();
+    }
   },
   mounted() {
-    // TODO
+    this.form.companyName = this.nameField;
   }
 }
 </script>
