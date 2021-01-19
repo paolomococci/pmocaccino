ad6b9570770ef9c42d52c7720d49418791425cc1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-19 08:40:37 +0100

    Update ViewTableComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/contest/ViewTableComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/contest/ViewTableComponent.vue
@@ -1,26 +1,87 @@
 <template>
   <section>
-    <h1 v-text="msg"></h1>
+    <!-- todo -->
   </section>
 </template>
 
 <script>
+import ContestRestService from '../services/ContestRestService'
+
 export default {
   name: 'ViewTableComponent',
-  props: {
-    msg: String
-  },
   data: () => ({
-    // TODO
+    fields: [
+      {
+        key: 'date',
+        label: 'dates',
+        sortable: true
+      },
+      {
+        key: 'name',
+        label: 'names'
+      },
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ],
+    contests: [],
+    messageBoxToConfirmDeletion: ''
   }),
   methods: {
-    // TODO
+    retrieveContests() {
+      ContestRestService.readAll()
+        .then(response => {
+          this.contests = response.data._embedded.contests;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    async updateView() {
+      this.$bvToast.toast('data of contests being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.contests = null;
+          this.retrieveContests();
+        }, 1000);
+    },
+    deleteItem(uri) {
+      ContestRestService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
+    },
+    deleteItemConfirm(uri) {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.deleteItem(uri);
+        }
+      });
+    }
   },
-  computed: {
-    // TODO
-  },
+  computed: {},
   mounted() {
-    // TODO
+    this.retrieveContests();
   }
 }
 </script>
