73da403d397b907ac8485e6262daf9cfe80601c5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-19 09:30:49 +0100

    Update DelRefToCompanyComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/DelRefToCompanyComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/DelRefToCompanyComponent.vue
@@ -5,24 +5,61 @@
 </template>
 
 <script>
-import EmployeeRestService from '../services/EmployeeRestService'
+import EmployeeRestService from '../../services/EmployeeRestService'
 
 export default {
   name: 'DelRefToCompanyComponent',
   props: {
-    // TODO
+    uri: String
   },
   data: () => ({
-    // TODO
+    company: ''
   }),
   methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
+    isAcceptable() {
+      return false;
+    },
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    },
+    retrieveCompany() {
+      EmployeeRestService.readCompanyMembership(this.uri)
+        .then(response => {
+          this.company = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    ruleOutCompanyMembership() {
+      this.hideModalDetail();
+      EmployeeRestService.deleteCompanyMembership(this.uri);
+    },
+    deleteConfirm() {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this reference', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.ruleOutCompanyMembership();
+        }
+      });
+    }
   },
   mounted() {
-    // TODO
+    this.retrieveCompany();
   }
 }
 </script>
