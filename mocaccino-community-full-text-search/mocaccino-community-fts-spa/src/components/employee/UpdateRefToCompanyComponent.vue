baec0ef1bec475351634fa02a8da05f19c105a1a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-19 09:31:16 +0100

    Update UpdateRefToCompanyComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/UpdateRefToCompanyComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/UpdateRefToCompanyComponent.vue
@@ -5,25 +5,68 @@
 </template>
 
 <script>
-import EmployeeRestService from '../services/EmployeeRestService'
-import CompanyRestService from '../services/CompanyRestService'
+import EmployeeRestService from '../../services/EmployeeRestService'
+import CompanyRestService from '../../services/CompanyRestService'
 
 export default {
   name: 'UpdateRefToCompanyComponent',
   props: {
-    // TODO
+    uri: String
   },
   data: () => ({
-    // TODO
+    company: '',
+    companies: []
   }),
-  methods: {
-    // TODO
-  },
   computed: {
-    // TODO
+    isRefSelected() {
+      return (this.company != '') ? false : true;
+    }
+  },
+  methods: {
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    },
+    retrieveCompanies() {
+      CompanyRestService.readAll()
+        .then(response => {
+          this.companies = response.data._embedded.companies;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    settingUpCompanyMembership() {
+      this.hideModalDetail();
+      var data = {
+        company: this.company
+      };
+      EmployeeRestService.updateReferenceToCompany(this.uri, data);
+    },
+    addConfirm() {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to add this reference', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'warning',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.settingUpCompanyMembership();
+        }
+      });
+    }
   },
   mounted() {
-    // TODO
+    this.retrieveCompanies();
   }
 }
 </script>
