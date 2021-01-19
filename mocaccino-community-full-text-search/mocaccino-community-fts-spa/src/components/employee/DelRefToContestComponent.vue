8b13545a73e09168f4bbd0d3d1d81301a8180cc5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-19 09:29:31 +0100

    Update DelRefToContestComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/DelRefToContestComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/DelRefToContestComponent.vue
@@ -5,24 +5,68 @@
 </template>
 
 <script>
-import EmployeeRestService from '../services/EmployeeRestService'
+import EmployeeRestService from '../../services/EmployeeRestService'
 
 export default {
   name: 'DelRefToContestComponent',
   props: {
-    // TODO
+    uri: String
   },
   data: () => ({
-    // TODO
+    contest: '',
+    contests: []
   }),
-  methods: {
-    // TODO
-  },
   computed: {
-    // TODO
+    isRefSelected() {
+      return (this.contest != '') ? false : true;
+    }
+  },
+  methods: {
+    showModalDetail() {
+      this.$refs['modal-edit-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-edit-reference'].hide();
+    },
+    retrieveContests() {
+      EmployeeRestService.readContestParticipation(this.uri)
+        .then(response => {
+          this.contests = response.data._embedded.contests;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    ruleOutContestParticipation() {
+      this.hideModalDetail();
+      var data = {
+        employeeUri: this.uri,
+        contestUri: this.contest
+      };
+      EmployeeRestService.deleteContestParticipation(data);
+    },
+    deleteConfirm() {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to remove this reference', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.ruleOutContestParticipation();
+        }
+      });
+    }
   },
   mounted() {
-    // TODO
+    this.retrieveContests();
   }
 }
 </script>
