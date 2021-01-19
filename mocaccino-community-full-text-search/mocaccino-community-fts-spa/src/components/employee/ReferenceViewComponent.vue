df8f44c8c58b3b9371a512b5f5499245dad208bc
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-19 09:31:46 +0100

    Update ReferenceViewComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/ReferenceViewComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/ReferenceViewComponent.vue
@@ -5,24 +5,67 @@
 </template>
 
 <script>
-import EmployeeRestService from '../services/EmployeeRestService'
+import EmployeeRestService from '../../services/EmployeeRestService'
 
 export default {
   name: 'ReferenceViewComponent',
   props: {
-    // TODO
+    uri: String
   },
   data: () => ({
-    // TODO
+    company: {"name":"","_links":{"self":{"href":""},"company":{"href":""},"employees":{"href":""}}},
+    contests: [],
+    fields: [
+      {
+        key: 'date',
+        sortable: true
+      },
+      {
+        key: 'name'
+      },
+      {
+        key: 'title'
+      },
+      {
+        key: 'description'
+      },
+      {
+        key: '_links.self.href',
+        label: 'URI'
+      }
+    ]
   }),
   methods: {
-    // TODO
-  },
-  computed: {
-    // TODO
+    showModalDetail() {
+      this.$refs['modal-view-reference'].show();
+    },
+    hideModalDetail() {
+      this.$refs['modal-view-reference'].hide();
+    },
+    retrieveCompany() {
+      EmployeeRestService.readCompanyMembership(this.uri)
+        .then(response => {
+          this.company = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    retrieveContests() {
+      EmployeeRestService.readContestParticipation(this.uri)
+        .then(response => {
+          this.contests = response.data._embedded.contests;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
   },
   mounted() {
-    // TODO
+    this.retrieveCompany();
+    this.retrieveContests();
   }
 }
 </script>
