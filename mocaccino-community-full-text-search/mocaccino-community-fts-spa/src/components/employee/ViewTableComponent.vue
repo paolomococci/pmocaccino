6aa4a80d8582fecc222448e7a280726d5e5b87c3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-01-19 09:32:14 +0100

    Update ViewTableComponent.vue

--- mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/ViewTableComponent.vue
+++ mocaccino-community-full-text-search/mocaccino-community-fts-spa/src/components/employee/ViewTableComponent.vue
@@ -5,24 +5,79 @@
 </template>
 
 <script>
-import EmployeeRestService from '../services/EmployeeRestService'
+import EmployeeRestService from '../../services/EmployeeRestService'
 
 export default {
   name: 'ViewTableComponent',
-  props: {
-    // TODO
-  },
   data: () => ({
-    // TODO
+    fields: [
+      {
+        key: 'username',
+        label: 'usernames',
+        sortable: true
+      },
+      {
+        key: 'showDetails',
+        label: 'details'
+      }
+    ],
+    employees: [],
+    messageBoxToConfirmDeletion: ''
   }),
   methods: {
-    // TODO
+    retrieveEmployees() {
+      EmployeeRestService.readAll()
+        .then(response => {
+          this.employees = response.data._embedded.employees;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    async updateView() {
+      this.$bvToast.toast('data of employees being updated', {
+        title: 'update view',
+        toaster: 'b-toaster-bottom-center',
+        variant: 'info',
+        solid: true,
+        appendToast: true,
+        autoHideDelay: 1000
+      });
+      await setTimeout(() => {
+          this.employees = null;
+          this.retrieveEmployees();
+        }, 1000);
+    },
+    deleteItem(uri) {
+      EmployeeRestService.delete(uri)
+        .catch(e => {
+          console.log(e);
+        });
+      this.updateView();
+    },
+    deleteItemConfirm(uri) {
+      this.messageBoxToConfirmDeletion = '';
+      this.$bvModal.msgBoxConfirm('are you sure you want to delete this item', {
+        title: 'please confirm',
+        size: 'md',
+        buttonSize: 'md',
+        okVariant: 'danger',
+        okTitle: 'yes',
+        cancelTitle: 'no',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      }).then(value => {
+        if(value) {
+          this.deleteItem(uri);
+        }
+      });
+    }
   },
-  computed: {
-    // TODO
-  },
+  computed: {},
   mounted() {
-    // TODO
+    this.retrieveEmployees();
   }
 }
 </script>
